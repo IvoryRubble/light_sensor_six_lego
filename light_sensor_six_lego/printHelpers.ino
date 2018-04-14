25e2da7d9556a6e90f49729aafba2bee9dc740f8
Yaroslav <none>
2018-04-14 12:41:01 +0700

    added print message function

--- light_sensor_six_lego/printHelpers.ino
+++ light_sensor_six_lego/printHelpers.ino
@@ -9,19 +9,27 @@ void printValues() {
 
 void printValuesNorm() {
   for (int i = 0; i < 6; i++) {
-    Serial.print(values[i]);
+    Serial.print(valuesNorm[i]);
     Serial.print("\t");
   }
   Serial.println();
 }
 
 void printCalibrationValues() {
-  char s[200];
+  char s[300];
   for (int i = 0; i < 6; i++) {
     sprintf(s, "valuesMin[%d] = %d\t\t valuesMax[%d] = %d \n", i, valuesMin[i], i, valuesMax[i]); 
     Serial.print(s);
   }
   Serial.flush();
+}
+
+void printMessage() {
+  for (int i = 0; i < 8; i++) {
+    Serial.print(message[i]);
+    Serial.print("\t");
+  }
+  Serial.println();
 }
 
 
